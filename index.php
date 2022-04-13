13d323cabb6032b34c1b376853a74078f4de5d68
Caleb Norris <norrisc129@gmail.com>
2022-04-13 18:19:46 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    //Create an instance of the Base class
+    $f3 = Base::instance();
+
+    //Run f3
+    $f3->run();
